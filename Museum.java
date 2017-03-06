45723b10ab2f4f3d4bb1faafa2896fea09247efa
kimnorth <benignhumour@gmail.com>
2017-03-06 15:49:29 +0000

    Created all files and completed a test to return the name of the Museum object.

---
+++ Museum.java
@@ -0,0 +1,13 @@
+class Museum {
+
+  private String name;
+
+  public Museum(String museum_name){
+    this.name = museum_name;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+}
---
+++ MuseumTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class MuseumTest {
+
+  Museum museum;
+
+  @Before
+  public void before() {
+    museum = new Museum("Natural History Museum");  
+  }
+
+  @Test
+  public void getBackName() {
+    assertEquals( "Natural History Museum", museum.getName() );   
+  }
+  
+
+}
